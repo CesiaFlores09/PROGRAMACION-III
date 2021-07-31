1e26a9d5c9fb167da910351d29f2af8774afbe44
Cesia Flores09 <florescsia@gmail.com>
2021-07-31 11:26:04 -0600

    fundamentos python

---
+++ fundamentos de python.py
@@ -0,0 +1,12 @@
+print("Hola Mundo")
+
+print("Bienvenidos  progra III")
+
+myname = "Cesia Flores"
+print("Mi nombre es: ", myname)
+
+
+Num1 = 5
+Num2 = 10
+resp= Num1 +  Num2
+print("La respues es: ", resp)
