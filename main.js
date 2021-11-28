7c0c3e26dffb50b70b3c46b2b05094f81def631c
Cesia Flores09 <florescsia@gmail.com>
2021-11-28 16:25:42 -0600

    Creacion de archivo turnos cambios en los otros archivos

--- main.js
+++ main.js
@@ -1,16 +1,19 @@
 window.zindex = 1;
+
 $(document).ready(function(e){
-    $("#mnxApp a").click(function(event){
+    $("#mnxAppClinica a").click(function(event){
         event.preventDefault();
 
         let modulo = $(this).data("modulo"),
             formulario = $(this).data("formulario");
         abrir_ventana(modulo, formulario);
     });
+    
 });
 function abrir_ventana(modulo, formulario){
-    console.log(`#${modulo}_${formulario}`, `${modulo}/${modulo}_${formulario}.html`);
+    // console.log(`http://localhost:3008/${modulo}/${modulo}_${formulario}.html`);
     $(`#${modulo}_${formulario}`).load(`${modulo}/${modulo}_${formulario}.html`)
+        .css("margin-top", "60px")
         .draggable()
         .click(function(e){
             $(this).css("z-index", ++zindex);
